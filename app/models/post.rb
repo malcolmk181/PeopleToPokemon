cde9c353d335d12b5a7a6fc31cbeeb7c14b29f95
Malcolm Keyes <3.malcolmk@gmail.com>
2022-07-04 11:14:40 -0400

    Added comments scaffold & added to post show page

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,11 @@
 class Post < ApplicationRecord
   belongs_to :person
   belongs_to :pokemon
+  has_many :comments
+
+  def likes
+    total = 0
+    comments.each {|c| c.vote ? total += 1 : total -= 1 }
+    total
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_04_135506) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_04_141814) do
+  create_table "comments", force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.string "content"
+    t.boolean "vote"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+  end
+
   create_table "people", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -33,6 +42,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_04_135506) do
     t.index ["pokemon_id"], name: "index_posts_on_pokemon_id"
   end
 
+  add_foreign_key "comments", "posts"
   add_foreign_key "posts", "people"
   add_foreign_key "posts", "pokemons"
 end
