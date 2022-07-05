60e5bc6ce80915789e15b197e1b6b5cad55b5144
Malcolm Keyes <3.malcolmk@gmail.com>
2022-07-05 12:24:24 -0400

    Improved new-post form pokemon dropdown Changed the dropdown so that it is a text field with an autocompleting dropdown as well. Currently just allows for selection of pokemon that already exist

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,4 +8,17 @@ class Post < ApplicationRecord
     comments.each {|c| c.vote ? total += 1 : total -= 1 }
     total
   end
+
+  def pokemon_name
+    if pokemon
+      pokemon.name
+    else
+      ""
+    end
+  end
+  
+  def pokemon_name=(name)
+    self.pokemon = Pokemon.find_by(name: name)
+    self.save
+  end
 end
