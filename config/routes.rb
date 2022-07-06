89ece3717c83ee67410747a368fdbc6c03e2739e
Malcolm Keyes <3.malcolmk@gmail.com>
2022-07-06 15:26:39 -0400

    added get / route -> posts#index

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   get "/posts/:id/comments/new", to: "comments#new"
   get "/posts/:id/comments/:comment_id/edit", to: "comments#edit"
+
+  get "/", to: "posts#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
